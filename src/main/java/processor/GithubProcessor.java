353ff6ea88f3f6da6296c716c34936881f729100
Chaowei Zeng <sli616@163.com>
2016-12-18 01:21:58 +0800

    refactor: separate GithubSpider

--- src/main/java/processor/GithubProcessor.java
+++ src/main/java/processor/GithubProcessor.java
@@ -2,32 +2,16 @@ package processor;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.pipeline.JsonFilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static processor.fetcher.*;
+import static processor.Fetcher.*;
 
-public class SinaBlogProcessor implements PageProcessor {
+public class GithubProcessor implements PageProcessor {
     private static int MAX_COLUMN_NUM = 10;
-    private static String START_URL_HEAD = "https://github.com/search?p=";
-    private static String START_URL_TAIL = "&q=location%3A%22Chengdu%22&ref=searchresults&type=Users&utf8=%E2%9C%93";
-    private static String START_PAGE_NUM = "1";
-    private static String START_URL = START_URL_HEAD + START_PAGE_NUM + START_URL_TAIL;
-    private static String ASSET_LOCATION = "/Users/cwzeng/Documents/WorkSpace/SpiderData";
-
-
-
-    public static void main(String[] args) {
-        Spider.create(new SinaBlogProcessor()).addUrl(START_URL).addPipeline(new JsonFilePipeline(ASSET_LOCATION))
-                .thread(5).run();
-    }
-
-
-    private Site site = Site.me().setSleepTime(100);
+    private Site site = Site.me().setSleepTime(8000);
 
     @Override
     public void process(Page page) {
---
+++ src/main/java/processor/GithubSpider.java
@@ -0,0 +1,20 @@
+package processor;
+
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+
+public class GithubSpider {
+
+    private static String LANGUAGE = "Java";
+    private static String START_URL_HEAD = "https://github.com/search?l=" + LANGUAGE + "&p=";
+    private static String START_URL_TAIL = "&q=location%3A%22Chengdu%22&ref=searchresults&type=Users&utf8=%E2%9C%93";
+    private static String START_PAGE_NUM = "1";
+    private static String START_URL = START_URL_HEAD + START_PAGE_NUM + START_URL_TAIL;
+    private static String ASSET_LOCATION = "/Users/cwzeng/Documents/WorkSpace/SpiderData/" + LANGUAGE;
+
+    public static void main(String[] args) {
+        Spider.create(new GithubProcessor()).addUrl(START_URL).addPipeline(new JsonFilePipeline(ASSET_LOCATION))
+                .thread(5).run();
+    }
+
+}
